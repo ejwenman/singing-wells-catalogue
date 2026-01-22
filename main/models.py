1e9dfafa82ff437cd127fd98e261b87095d83db5
ejwenman <ejwenman@gmail.com>
2026-01-22 20:27:17 +0000

    Fix archive_id max_length error

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class FieldTrip(models.Model):
-    archive_id = models.CharField(max_length=3, unique=True)
+    archive_id = models.CharField(max_length=50, unique=True)
     year = models.IntegerField()
     country = models.CharField(max_length=50)
     region = models.CharField(max_length=255, blank=True, null=True)
